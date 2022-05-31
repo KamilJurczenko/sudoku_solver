059ddedd9766f982eda8cf5c5d04f96ae7024e72
KamilJurczenko <kamil.jurczenko@fh-bielefeld.de>
2022-05-31 13:43:12 +0200

    dokumentation und seriellen Code geupdated
    sudoku_seriell: startsudokus cachen (nicht getestet)
    doc.md: Strategie zur parallelisierung formuliert

--- doc/doc.md
+++ doc/doc.md
@@ -8,7 +8,7 @@ Sprecher:
 
 
 Dokumentation:
-	Ein Sudokufeld wird mithilfe des Backtracking Algorithmus gelöst. Hierfür wird sudoku_seriell.c von "Pradeep Mondal P" verwendet, wobei ein Sudokufeld als 9x9 Matrix betrachtet wird.
+	Ein Sudokufeld wird mithilfe des Backtracking Algorithmus (äquivalent zu einer Tiefensuche eines Graphens) gelöst. Hierfür wird sudoku_seriell.c von "Pradeep Mondal P" verwendet [2], wobei ein Sudokufeld als 9x9 Matrix betrachtet wird.
 	Der Algorithmus verläuft folglich als Pseudocode:
 	
 	(1) solveSudoku aufrufen
@@ -18,18 +18,31 @@ Dokumentation:
 			(4.1) Wenn legal gebe 1 aus und gehe eine Spalte weiter und führe (1) rekursiv aus
 			(4.2) Gebe 0 aus falls keine Lösung gefunden
 	
-	Dieser Algorithmus lässt sich als eine Tiefensuche in einem Graphen vergleichen. Da dieser rekursiv verläuft, wird eine Parallelisierung der Lösungsfindung problematisch.
+	Dieser Algorithmus hat im schlimmsten Fall eine exponentielle Laufzeit von z^N und ist dementsprechend sehr Aufwendig.
+	Dieses Problem lässt sich unter Umständen parallelisieren. Da der Algorithmus rekursiv verläuft, wird eine Parallelisierung der Lösungsfindung problematisch, weshalb alternative Lösungen gefunden werden müssen.
 	Um dieses Problem zu lösen wurden zwei Gruppen gebildet die sich unabhängig voneinander eine Strategie zur Parallelisierung entwickelten.
+	Es wurden folgende Strategien entwickelt:
 	
 	Strategien:
-		(1) Bildung von Teilsudokus für je ein Prozess (siehe Grupp1 Branch im git)
-		    Tiefensuche wird zur Breitensuche, was sich parallelisieren lässt
-				- Der serielle Code wurde modifiziert
+		Knoten := Startsudokus
+		(1) Bildung von Startsudokus für je ein Prozess (siehe Grupp1 Branch im git)
+				- Der serielle Code wurde dafür modifiziert
+					- Startsudokus werden erstellt, indem eine Breitensuche n Prozesse mal, vor der eigentlichen Parallelisierung, durchgeführt wird. In jedem Durchlauf wird ein
+					  Startsudoku gefunden welcher einem Prozess zugewiesen wird. Es musst so oft gesucht werden, um eine Tiefe = Prozessanzahl im Graphen zu erreichen.
+				- Falls eine Lösung in einem beliebigen Prozess gefunden wurde, werden die restlichen Prozesse frühzeitig abgebrochen und das Programm wird beendet.
+				  Das gelöste Sudoku und die benötigte Zeit für die Berechnung wird ausgegeben.
+			mögliche Probleme:
+				- Was macht ein Prozess der frühzeitig abgebrochen wird, weil er keine Lösung gefunden hat?
+					- Vermutung: Sollte nur bei sehr vielen Prozessen problematisch sein.
+				- Es werden eine maximale Anzahl von Prozessen benötigt, weil beispielsweise in einem Sudoku nur maximal 50 Knoten in einer Tiefe im Graphen vorkommen können[3].
+			Probleme:
+				- Wenn die Anzahl von Knoten in einer Tiefe im Graphen niemals genau mit der angegebennen Prozessanzahl zustimmt,
+					-> Nehme die erste Tiefe die mindestens soviele Knoten hat wie Anzahl Prozesse und ignoriere so lange die nicht zugewiesenen Knoten
+					   bis ein Prozess keine Lösung findet und die nicht zugewiesenen Knoten weitervearbeiten kann.
+				- Breitensuche könnte unter Umständen viel dynamischen Speicher verbrauchen
 			
 			Optimierungen:
 			
-			Probleme:
-				Ein Prozess könnte schnell zu keiner Lösung kommen und es muss ein neues Teilsudoku zugewiesen werden.
 		
 		
 
@@ -37,4 +50,4 @@ Dokumentation:
 Referenzen:
 [1]https://github.com/vduan/parallel-sudoku-solver
 [2]https://www.geeksforgeeks.org/sudoku-backtracking-7/
-
+[3]https://slideshare.net/FernandoJunior52/bdfsdfspaper
--- source/project/sudoku_seriell.c
+++ source/project/sudoku_seriell.c
@@ -2,19 +2,35 @@
 
 Project:
 
-Original authors:
+Modified by:
 (sorted by lastname)
 	Daitche Konstantin
 	Jurczenko Kamil
+
+Original Author:
+	Pradeep Mondal P
 
 *********************************************************************/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <stdbool.h>
 
 // N is the size of the 2D matrix N*N
 #define N 9
+
+// global variables
+
+int grid[N][N] = { { 3, 0, 6, 5, 0, 8, 4, 0, 0 },
+			   { 5, 2, 0, 0, 0, 0, 0, 0, 0 },
+			   { 0, 8, 7, 0, 0, 0, 0, 3, 1 },
+			   { 0, 0, 3, 0, 1, 0, 0, 8, 0 },
+			   { 9, 0, 0, 8, 6, 3, 0, 0, 5 },
+			   { 0, 5, 0, 0, 9, 0, 6, 0, 0 },
+			   { 1, 3, 0, 0, 0, 0, 2, 5, 0 },
+			   { 0, 0, 0, 0, 0, 0, 0, 7, 4 },
+			   { 0, 0, 5, 2, 0, 6, 3, 0, 0 } };
 
 /* A utility function to print grid */
 void print(int arr[N][N])
@@ -65,9 +81,8 @@ to assign values to all unassigned locations in
 such a way to meet the requirements for
 Sudoku solution (non-duplication across rows,
 columns, and boxes) */
-int solveSudoku(int grid[N][N], int row, int col)
+int solveSudoku(int grid[N][N], int row, int col, bool returnOnFind, int **grid_ptr)
 {
-
 	// Check if we have reached the 8th row
 	// and 9th column (0
 	// indexed matrix) , we are
@@ -89,11 +104,13 @@ int solveSudoku(int grid[N][N], int row, int col)
 	// of the grid already contains
 	// value >0, we iterate for next column
 	if (grid[row][col] > 0)
-		return solveSudoku(grid, row, col + 1);
+		return solveSudoku(grid, row, col + 1, returnOnFind, grid_ptr);
+
+	if (returnOnFind)
+		*grid_ptr = malloc(sizeof(int) * N);
 
 	for (int num = 1; num <= N; num++)
 	{
-
 		// Check if it is safe to place
 		// the num (1-9) in the
 		// given row ,col ->we move to next column
@@ -107,9 +124,17 @@ int solveSudoku(int grid[N][N], int row, int col)
 			is correct	 */
 			grid[row][col] = num;
 
+			// Cache grid if starting Grid needed and return to deny backtracking
+			if (returnOnFind)
+			{
+				*grid_ptr[num - 1] = &grid;
+				free(grid_ptr);
+				continue;
+			}
+
 			// Checking for next possibility with next
 			// column
-			if (solveSudoku(grid, row, col + 1) == 1)
+			if (solveSudoku(grid, row, col + 1, false, NULL) == 1)
 				return 1;
 		}
 
@@ -123,7 +148,39 @@ int solveSudoku(int grid[N][N], int row, int col)
 	return 0;
 }
 
+// ToDo Get Array of starting Sudoku Grids
+int** getStartSudokus(int num)
+{
+	//int tmpGrid[N][N] = grid;
+	int *tmpGrid_ptr = malloc(sizeof(int));
+	tmpGrid_ptr = &grid;
+	int tmpGridLength = sizeof(tmpGrid_ptr);
+	int depth = -1; // counter val for current depth of breadth search
+	int breadth = 1;
 
+	// breadth search till breadth = num or breadth < num
+	while (breadth <= num) 
+	{
+		int *gridArr;
+		//tmpGrid_ptr = calloc(500, sizeof(grid));
+		for (int i = 0; i < tmpGridLength; i++)
+		{
+			solveSudoku(*(tmpGrid_ptr + i), 0, 0, true, &gridArr); // ToDo Cache rows,cols for better performance
+			for (int j = 0; j < sizeof(gridArr); j++) 
+			{
+				*(tmpGrid_ptr + j) = *(gridArr + j);
+			}
+		}
+		tmpGridLength = sizeof(tmpGrid_ptr);
+		breadth = sizeof(gridArr);
+		depth++;
+		if (depth == 500) break; // for debugging
+	}
+
+	//free(tmpGrid_ptr);
+	return tmpGrid_ptr;
+}
+ 
 //Abwandlung von solveSudoku.
 //Printet alle möglichen Spielfeld Kombination für maxStellen aus.
 int teilSudokus(int grid[N][N], int row, int col, int maxStellen, int stelle)
@@ -192,15 +249,7 @@ int teilSudokus(int grid[N][N], int row, int col, int maxStellen, int stelle)
 int main()
 {
 	// 0 means unassigned cells
-	int grid[N][N] = { { 3, 0, 6, 5, 0, 8, 4, 0, 0 },
-				   { 5, 2, 0, 0, 0, 0, 0, 0, 0 },
-				   { 0, 8, 7, 0, 0, 0, 0, 3, 1 },
-				   { 0, 0, 3, 0, 1, 0, 0, 8, 0 },
-				   { 9, 0, 0, 8, 6, 3, 0, 0, 5 },
-				   { 0, 5, 0, 0, 9, 0, 6, 0, 0 },
-				   { 1, 3, 0, 0, 0, 0, 2, 5, 0 },
-				   { 0, 0, 0, 0, 0, 0, 0, 7, 4 },
-				   { 0, 0, 5, 2, 0, 6, 3, 0, 0 } };
+
 
 	//if (solveSudoku(grid, 0, 0) == 1)
 	//	print(grid);
@@ -209,7 +258,6 @@ int main()
 
 	//return 0;
 
-	teilSudokus(grid, 0, 0, 4, 1);
+	//teilSudokus(grid, 0, 0, 45, 1);
 
-	// This is code is contributed by Pradeep Mondal P
 }
